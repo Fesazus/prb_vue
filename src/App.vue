da588d93fc930632b941f611ad46c3f0805159e9
Fesazus <mail@eurador.de>
2022-04-22 10:38:48 +0200

    back4app

--- src/App.vue
+++ src/App.vue
@@ -52,11 +52,42 @@
 		</div>
 	</div>
 </template>
-
 <script>
-/* This is the minified production version of Parse JS  */
-/* type = "text/javascript";
-src = "https://npmcdn.com/parse/dist/parse.min.js"; */
+/* ##### Datenbankanbindung #### */
+
+// Initialize Parse
+/* Parse.initialize(
+	"ACZFwLfcjciL0G4bgtGSGloBIuXyMYucSBFrp4Er",
+	"ALxepfxOexSgKIiQHlFOV0YSJ4EKxiyRIPHC8ErH"
+); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = "https://parseapi.back4app.com/";
+let recipyTable = Parse.Object.extend("Recipy"); */
+
+/* ##### Test #### */
+
+/* let query = new Parse.Query(recipyTable);
+query.equalTo("Title", "BBQ");
+query
+	.first()
+	.then(function (recipy) {
+		console.log("recipy");
+		console.log(recipy);
+		console.log(typeof recipy);
+		console.log(recipy instanceof Array);
+		if (recipy) {
+			console.log(
+				"Pet found successful with name: " +
+					recipy.get("Title") +
+					" and age: " +
+					recipy.get("Category")
+			);
+		} else {
+			console.log("Nothing found, please try again");
+		}
+	})
+	.catch(function (error) {
+		console.log("Error: " + error.code + " " + error.message);
+	}); */
 
 import imageRecipy from "./assets/img/Okraschoteneintopf.jpg";
 
@@ -73,6 +104,15 @@ export default {
 		return {
 			image: imageRecipy,
 		};
+	},
+	mounted() {
+		// Datenbank
+		let Script = document.createElement("script");
+		Script.setAttribute(
+			"src",
+			"https://npmcdn.com/parse/dist/parse.min.js"
+		);
+		document.head.appendChild(Script);
 	},
 };
 </script>
