c26c1efa758628942faba6929a7304485437a0ee
Fesazus <mail@eurador.de>
2022-04-26 11:24:56 +0200

    dataBase.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<dataBase></dataBase>
 		<h1>Rezepte</h1>
 		<input
 			type="button"
@@ -38,8 +39,7 @@
 import recipyList from "./components/recipyList.vue";
 import recipyContainer from "./components/recipyContainer.vue";
 import searchInRecipyList from "./components/searchInRecipyList.vue";
-
-import Parse from "parse/dist/parse.min.js";
+import dataBase from "./components/dataBase.vue";
 
 export default {
 	name: "App",
@@ -47,6 +47,7 @@ export default {
 		recipyList,
 		recipyContainer,
 		searchInRecipyList,
+		dataBase,
 	},
 	data() {
 		return {
@@ -98,53 +99,6 @@ export default {
 			],
 			categoryItems: ["Paprika-Eintopf (Bamja)", "Baloc"],
 		};
-	},
-	mounted() {
-		// Datenbank
-		let Script = document.createElement("script");
-		Script.setAttribute(
-			"src",
-			"https://npmcdn.com/parse/dist/parse.min.js"
-		);
-		document.head.appendChild(Script);
-		// Initialize Parse
-		Parse.initialize(
-			"ACZFwLfcjciL0G4bgtGSGloBIuXyMYucSBFrp4Er",
-			"ALxepfxOexSgKIiQHlFOV0YSJ4EKxiyRIPHC8ErH"
-		); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
-		Parse.serverURL = "https://parseapi.back4app.com/";
-		let recipyTable = Parse.Object.extend("Recipy");
-		/* ##### Test #### */
-		let query = new Parse.Query(recipyTable);
-		query.equalTo("Title", "BBQ");
-		query
-			.first()
-			.then(function (recipy) {
-				console.log("recipy");
-				console.log(recipy);
-				console.log(typeof recipy);
-				console.log(recipy instanceof Array);
-				if (recipy) {
-					console.log(
-						"Pet found successful with name: " +
-							recipy.get("Title") +
-							" and age: " +
-							recipy.get("Category")
-					);
-				} else {
-					console.log("Nothing found, please try again");
-				}
-			})
-			.catch(function (error) {
-				console.log("Error: " + error.code + " " + error.message);
-			});
-		/* 		let queryAll = new Parse.Query(recipyTable);
-		queryAll
-			.findAll()
-			.then(function (recipies) {})
-			.catch(function (error) {
-				console.log("Error: " + error.code + " " + error.message);
-			}); */
 	},
 };
 </script>
--- src/components/database.vue
+++ src/components/database.vue
@@ -3,7 +3,57 @@
 </template>
 
 <script>
-export default {};
+import Parse from "parse/dist/parse.min.js";
+export default {
+	name: "dataBase",
+	mounted() {
+		// Datenbank
+		let Script = document.createElement("script");
+		Script.setAttribute(
+			"src",
+			"https://npmcdn.com/parse/dist/parse.min.js"
+		);
+		document.head.appendChild(Script);
+		// Initialize Parse
+		Parse.initialize(
+			"ACZFwLfcjciL0G4bgtGSGloBIuXyMYucSBFrp4Er",
+			"ALxepfxOexSgKIiQHlFOV0YSJ4EKxiyRIPHC8ErH"
+		); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+		Parse.serverURL = "https://parseapi.back4app.com/";
+		let recipyTable = Parse.Object.extend("Recipy");
+		/* ##### Test #### */
+		let query = new Parse.Query(recipyTable);
+		query.equalTo("Title", "BBQ");
+		query
+			.first()
+			.then(function (recipy) {
+				console.log("recipy");
+				console.log(recipy);
+				console.log(typeof recipy);
+				console.log(recipy instanceof Array);
+				if (recipy) {
+					console.log(
+						"Pet found successful with name: " +
+							recipy.get("Title") +
+							" and age: " +
+							recipy.get("Category")
+					);
+				} else {
+					console.log("Nothing found, please try again");
+				}
+			})
+			.catch(function (error) {
+				console.log("Error: " + error.code + " " + error.message);
+			});
+		/* 		let queryAll = new Parse.Query(recipyTable);
+		queryAll
+			.findAll()
+			.then(function (recipies) {})
+			.catch(function (error) {
+				console.log("Error: " + error.code + " " + error.message);
+			}); */
+	},
+};
 </script>
 
 <style>
