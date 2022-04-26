b9c897f58546785b0e0ebf98d156a51e1a1e801f
Fesazus <mail@eurador.de>
2022-04-26 15:04:46 +0200

    recipy container ohne list with headline

--- src/components/recipyContainer.vue
+++ src/components/recipyContainer.vue
@@ -2,35 +2,30 @@
 	<div>
 		<shoppingItems :ingredients="ingredients"></shoppingItems>
 		<div class="left">
-			<listWithHeadline></listWithHeadline>
 			<p>{{ amount }}</p>
-			<ul>
-				<li
-					v-for="(instructionListitem, index) in instructions"
-					:key="index"
-				>
-					<h3>{{ instructionListitem[0] }}</h3>
-					<ul>
-						<li
-							v-for="(item, index) in instructionListitem[1]"
-							:key="index"
-						>
-							{{ item }}
-						</li>
-					</ul>
-				</li>
-			</ul>
+			<div
+				v-for="(instructionListitem, index) in instructions"
+				:key="index"
+			>
+				<h3>{{ instructionListitem[0] }}</h3>
+				<ul>
+					<li
+						v-for="(item, index) in instructionListitem[1]"
+						:key="index"
+					>
+						{{ item }}
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import listWithHeadline from "./listWithHeadline.vue";
 import shoppingItems from "./shoppingItems.vue";
 export default {
 	name: "recipyContainer",
 	components: {
-		listWithHeadline,
 		shoppingItems,
 	},
 	data() {
