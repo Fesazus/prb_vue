1c99764f7bb44bad08249a2d67013396d9ff2279
Fesazus <mail@eurador.de>
2022-05-02 23:27:25 +0200

    css aufteilen teil 2

--- src/components/shoppingItems.vue
+++ src/components/shoppingItems.vue
@@ -41,4 +41,21 @@ li {
 	width: 33%;
 	margin: 4mm 10mm 0 0;
 }
+
+.boxed {
+	border: 1px solid #ffffe6;
+	box-shadow: 5px 5px 10px #bababa;
+	border-radius: 10px;
+	background-color: #ffffe6;
+}
+
+.boxed li {
+	list-style-type: none;
+	padding: 5px 0 0 0;
+	margin: 0;
+}
+
+.boxed li:last-of-type {
+	padding: 5px 0 10px 0;
+}
 </style>
